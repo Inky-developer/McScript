9af08e8e835f1fafdd402578ee47d485d6a935e9
DavidVenhoff <47245667+DavidVenhoff@users.noreply.github.com>
2020-04-12 18:28:00 +0200

    update .travis.yml

--- test/__main__.py
+++ test/__main__.py
@@ -1,4 +1,21 @@
 import unittest
 
+# noinspection PyUnresolvedReferences
+from .Language.TestMcLang import TestMcLang
+# noinspection PyUnresolvedReferences
+from .TestAddress import TestAddress
+# noinspection PyUnresolvedReferences
+from .TestBiomes import TestBiomes
+# noinspection PyUnresolvedReferences
+from .TestDataReader import TestDataReader
+# noinspection PyUnresolvedReferences
+from .TestFunctionSignature import TestFunctionSignature
+# noinspection PyUnresolvedReferences
+from .TestNamespace import TestNamespace
+# noinspection PyUnresolvedReferences
+from .TestStringResource import TestStringResource
+# noinspection PyUnresolvedReferences
+from .TestUtils import TestUtils
+
 if __name__ == '__main__':
     unittest.main()
